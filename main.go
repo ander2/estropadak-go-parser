bafed49d304dbbe477fd23e39df3acd3a0aeb017
Ander Garmendia <kelertxiki@gmail.com>
2024-02-04 18:55:48 +0100

    Add flag to select parser

--- main.go
+++ main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 	"io"
+	"os"
 	"sort"
 	"text/tabwriter"
-)
 
-import "estropadak.eus/estropadak-parser/parsers"
+	"estropadak.eus/estropadak-parser/parsers"
+)
 
 func main() {
 	var estropada estropadakParser.Estropada
+	var err error
+
+	typePtr := flag.String("t", "ACT", "Parser type: ACT or ARC")
+	flag.Parse()
 
 	estropada = estropadakParser.Estropada{}
 	reader := io.Reader(os.Stdin)
-	_, err := estropadakParser.Act_parse_estropadak_doc(&estropada, reader)
+	if *typePtr == "ACT" {
+		_, err = estropadakParser.Act_parse_estropadak_doc(&estropada, reader)
+	} else if *typePtr == "ARC" {
+		_, err = estropadakParser.Arc_parse_estropadak_doc(&estropada, reader)
+	} else {
+		fmt.Fprintln(os.Stderr, "Invalid parser type: select ACT or ARC")
+		os.Exit(1)
+	}
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Cannot parse doc %s", err)
 		os.Exit(1)
@@ -24,7 +36,7 @@ func main() {
 }
 
 func format_result(estropada estropadakParser.Estropada) {
-    sort.Sort(estropadakParser.ByPosition(estropada.Results))
+	sort.Sort(estropadakParser.ByPosition(estropada.Results))
 	w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
 	fmt.Fprintf(w, "\t%s\n", estropada.Name)
 	for _, res := range estropada.Results {
