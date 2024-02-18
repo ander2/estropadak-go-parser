e58eeec078c9abb8525fb13ddeedc1498c9e77c4
Ander Garmendia <kelertxiki@gmail.com>
2024-02-18 19:12:33 +0100

    Allow downloading and parsing content with -u flag

--- cmd/estropadakparser.go
+++ cmd/estropadakparser.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 	"sort"
 
@@ -14,12 +15,28 @@ import (
 func main() {
 	var estropada estropadakParser.Estropada
 	var err error
+	var reader io.Reader
 
 	typePtr := flag.String("t", "ACT", "Parser type: ACT or ARC")
+	urlPtr := flag.String("u", "", "Content URL: http://www.liga-arc.com/es/regata/489/xvii.-hondarribiko-arrantzaleen-kofradiko-bandera")
 	flag.Parse()
 
 	estropada = estropadakParser.Estropada{}
-	reader := io.Reader(os.Stdin)
+	if *urlPtr == "" {
+		reader = io.Reader(os.Stdin)
+	} else {
+		resp, err := http.Get(*urlPtr)
+		if err != nil {
+			fmt.Printf("Cannot fetch content on %s: %s\n", *urlPtr, err)
+			os.Exit(1)
+		}
+		if resp.StatusCode != 200 {
+			fmt.Printf("Cannot fetch content on %s: %s\n", *urlPtr, resp.Status)
+			os.Exit(1)
+		}
+		defer resp.Body.Close()
+		reader = resp.Body
+	}
 	if *typePtr == "ACT" {
 		_, err = estropadakParser.Act_parse_estropadak_doc(&estropada, reader)
 	} else if *typePtr == "ARC" {
