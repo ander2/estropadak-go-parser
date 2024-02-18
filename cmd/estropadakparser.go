be6f3a7fa0ed0c2af69057b3cc7cbd8aad3f0626
Ander Garmendia <kelertxiki@gmail.com>
2024-02-18 19:56:46 +0100

    Update description

--- cmd/estropadakparser.go
+++ cmd/estropadakparser.go
@@ -19,7 +19,7 @@ func main() {
 
 	typePtr := flag.String("t", "ACT", "Parser type: ACT or ARC")
 	urlPtr := flag.String("u", "", "Content URL: http://www.liga-arc.com/es/regata/489/xvii.-hondarribiko-arrantzaleen-kofradiko-bandera")
-	formatPtr := flag.String("f", "", "Output format: text or json")
+	formatPtr := flag.String("f", "", "Output format: text, yaml or json")
 	flag.Parse()
 
 	estropada = estropadakParser.Estropada{}
