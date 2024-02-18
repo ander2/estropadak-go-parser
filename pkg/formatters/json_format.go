32857f84669bab7e9db5be481d80dba550afc1fa
Ander Garmendia <kelertxiki@gmail.com>
2024-02-18 19:53:53 +0100

    Add error control

--- pkg/formatters/json_format.go
+++ pkg/formatters/json_format.go
@@ -3,11 +3,16 @@ package formatters
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 
 	"estropadak.eus/estropadak-parser/pkg/parsers"
 )
 
 func Format_result_json(estropada estropadakParser.Estropada) {
-	data, _ := json.MarshalIndent(estropada, "", "  ")
+	data, err := json.MarshalIndent(estropada, "", "  ")
+	if err != nil {
+		fmt.Printf("Cannot marshal into json format: %s\n", err)
+		os.Exit(1)
+	}
 	fmt.Printf("%s\n", data)
 }
